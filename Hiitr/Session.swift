7d66e49d0e53b9927a99a354a0d98760750d99a2
William Mora <wilmor24@gmail.com>
2015-08-02 17:31:19 -0300

    countdown timer from 30 to 0

---
+++ Hiitr/Session.swift
@@ -0,0 +1,63 @@
+//
+//  Session.swift
+//  Hiitr
+//
+//  Created by William Mora on 2/8/15.
+//  Copyright (c) 2015 nispok. All rights reserved.
+//
+
+import Foundation
+
+protocol SessionEvents {
+    func sessionStarted()
+    func sessionUpdate(timeLeft: Int)
+    func sessionStopped()
+}
+
+class Session : NSObject {
+    
+    let workTime = 30
+    let timerInterval: NSTimeInterval = 1
+    
+    var timer: NSTimer?
+    var elapsedSeconds: NSInteger
+    var delegate: SessionEvents?
+    
+    override init() {
+        elapsedSeconds = 0
+    }
+    
+    convenience init(delegate: SessionEvents) {
+        self.init()
+        self.delegate = delegate
+    }
+    
+    func start() {
+        if (timer != nil) {
+            timer?.invalidate()
+        }
+        timer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector: "timerUpdate:", userInfo: nil, repeats: true)
+        elapsedSeconds = 0
+        timer?.fire()
+        delegate?.sessionStarted()
+        print("session started")
+    }
+    
+    func stop() {
+        timer?.invalidate()
+        delegate?.sessionStopped()
+        print("session stopped")
+    }
+    
+    func timerUpdate(timer: NSTimer) {
+        delegate?.sessionUpdate(self.timeLeft())
+        ++elapsedSeconds
+        if (elapsedSeconds > workTime) {
+            self.stop()
+        }
+    }
+    
+    func timeLeft() -> NSInteger {
+        return workTime - elapsedSeconds
+    }
+}
--- Hiitr/ViewController.swift
+++ Hiitr/ViewController.swift
@@ -8,25 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SessionEvents {
     
     @IBOutlet weak var controlButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     
     var currentState: SessionState
+    var session: Session?
     
     required init(coder aDecoder: NSCoder) {
         currentState = .Stopped
         super.init(coder: aDecoder)
+        session = Session(delegate: self)
     }
 
     @IBAction func controlButtonPressed(sender: AnyObject) {
-        currentState = currentState == .Stopped ? .Started: .Stopped
+        switch currentState {
+        case SessionState.Stopped:
+            session?.start()
+            currentState = .Started
+        case SessionState.Started:
+            session?.stop()
+            currentState = .Stopped
+        }
+        updateTimerLabel()
+    }
+    
+    func updateTimerLabel() {
         controlButton.setTitle(currentState == .Stopped ? "Start" : "Stop", forState: UIControlState.Normal)
     }
     
-    func updateLabel() {
-        
+    func sessionStarted() {
+        updateTimerLabel()
+    }
+    
+    func sessionStopped() {
+        updateTimerLabel()
+    }
+    
+    func sessionUpdate(timeLeft: Int) {
+        timerLabel.text = "\(timeLeft)"
     }
 
 }
