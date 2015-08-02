dc75f32ff3319172b8be3308794f175fd8084ed0
William Mora <wilmor24@gmail.com>
2015-08-02 19:22:01 -0300

    interval mode enum

---
+++ Hiitr/IntervalMode.swift
@@ -0,0 +1,11 @@
+//
+//  IntervalMode.swift
+//  Hiitr
+//
+//  Created by William Mora on 2/8/15.
+//  Copyright © 2015 nispok. All rights reserved.
+//
+
+enum IntervalMode {
+    case Work, Rest
+}
